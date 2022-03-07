833337b4f8f5d1f9dd0a56cd8a1b51c30bf05a92
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-03-07 15:28:35 -0300

    Requisito 1

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">não encontrado</div>
+    );
+  }
+}
+
+export default NotFound;
