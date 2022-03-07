68a790c35136688ea8292ca95c2a6fbf1f7874cd
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-03-07 16:01:54 -0300

    Requisito 1 corrigido e add coisas que faltaram

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
-import NotFound from './NotFound';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route exact path="/search">
-          <Search />
-        </Route>
-        <Route exact path="/album/:id">
-          <Album />
-        </Route>
-        <Route exact path="/favorites">
-          <Favorites />
-        </Route>
-        <Route exact path="/profile">
-          <Profile />
-        </Route>
-        <Route exact path="/profile/edit">
-          <ProfileEdit />
-        </Route>
-        <Route exact path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+          <Route exact path="/album/:id">
+            <Album />
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/profile/edit">
+            <ProfileEdit />
+          </Route>
+          <Route exact path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <input data-testid="login-name-input" placeholder="Insira seu nome"></input>
+    </div>
     );
   }
 }
