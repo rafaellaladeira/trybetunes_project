a24a86732f85a0284080e6bad06f03eb23dc8a66
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-03-08 12:13:14 -0300

    Requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,29 +8,34 @@ class Header extends React.Component {
     super();
 
     this.state = {
+      name: '',
       isLoading: false,
     };
+  }
+
+  componentDidMount() {
+    this.fetchName();
   }
 
   fetchName = async () => {
     this.setState({
       isLoading: true,
     });
-
-    await getUser();
+    const response = await getUser();
     this.setState({
+      name: response.name,
       isLoading: false,
     });
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { name, isLoading } = this.state;
     return (
       isLoading
         ? <Loading />
         : (
           <header data-testid="header-component">
-            <div data-testid="header-user-name">{ this.fetchName }</div>
+            <p data-testid="header-user-name">{ name }</p>
             <Link to="/search" data-testid="link-to-search"> Search </Link>
             <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
             <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
